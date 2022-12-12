02a79721b300ac16e3d7994fccc7aa7511f2bb8c
Milosz Matuszewski <inzynier.matuszewski@gmail.com>
2022-12-12 18:49:47 +0100

    minor tweaks

--- Python/kielbasa.py
+++ Python/kielbasa.py
@@ -3,22 +3,23 @@ from datetime import datetime
 import time
 import math
   
-  
-
-
+# Authenticate to Salesforce
 username='inzynier.matuszewski@playful-badger-u49kep.com'
-password='yukka314'
-token='5HIIMalRkhRYilPxmKbn3UCb'
+password='majeranek8'
+token='WAOCeEHUo33ArmXAlBox5hGTY'
 sf = Salesforce(username=username, password=password, security_token=token)
+
 
 for i in range(0,100):
 
     # returns current date and time
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%dT%H:%M:%S")
-    print("Timestamp = ", timestamp)
 
-    sf.Temperature__c.create({'Temperature__c':math.sin(i), 'Timestamp__c': timestamp})
+    temperature = math.sin(i)*100
+    print("Time: %s, Temperature: %2d"%(timestamp, temperature))
+
+    sf.Temperature__c.create({'Temperature__c':temperature, 'Timestamp__c': timestamp})
     print(sf)
     time.sleep(10)
 
